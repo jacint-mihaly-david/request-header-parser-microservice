0cdde363fbd706f9926409eae9bd903b6f7df3db
jacint-mihaly-david <107406590+jacint-mihaly-david@users.noreply.github.com>
2023-09-11 22:14:54 +0000

    chore: [FCC-BE-2] - finished Request Header Parser Microservice project

--- index.js
+++ index.js
@@ -15,16 +15,24 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/api/whoami', (req, res) => {
+  console.log('received request');
+  const {
+    ['x-forwarded-for']: ipaddress,
+    ['accept-language']: language,
+    ['user-agent']: software
+  } = req.headers;
+  const response = { ipaddress, language, software };
+  res.json(response);
+  return;
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
